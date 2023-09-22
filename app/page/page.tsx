69f493f30f3eb38fdae1229ea15bb92153562569
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-09-21 19:46:34 -0700

    got a working build

--- app/page/page.tsx
+++ app/page/page.tsx
@@ -1,8 +1,8 @@
 import 'server-only'
 
-export const metdata = {
-  title: 'App Router',
-}
+// export const metdata = {
+//   title: 'App Router',
+// }
 
 export default function Page() {
   return <h1>App Router</h1>
