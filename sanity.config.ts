3ad6d7ec591ccb53510ca7cbde60528ed4596510
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-09-21 19:16:02 -0700

    updating

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
+import {apiVersion, dataset, projectId} from './studio/env'
+import {schema} from './studio/schema'
 
 export default defineConfig({
   basePath: '/studio',
