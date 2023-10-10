79dbad91ae25bbdd142e0a3798000ebb98cb8f4d
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-10-09 18:48:31 -0700

    updating package json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,8 @@
-import Head from 'next/head';
 import Image from 'next/image';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<Head>
-				<title>Create Next App</title>
-				<link
-					rel="icon"
-					href="/favicon.ico"
-				/>
-			</Head>
-
 			<body>
 				<header>
 					<h1>HELLO</h1>
