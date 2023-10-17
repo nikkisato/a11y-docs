8e4e1791ccc06dd7d0df75f559961dc8a01e2980
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-10-16 18:47:33 -0700

    got Material UI and Icon Global

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,23 @@
 import '../styles/reset.css';
 import '../styles/global.css';
+import '../styles/icon.css';
 import Container from '../src/components/Container/Container';
 import Sidebar from 'src/components/Sidebar/Sidebar';
 import Content from 'src/components/Content/Content';
+import Header from '../src/layouts/Header/Header';
+import Footer from '../src/layouts/Footer/Footer';
+
+// TODO: ASK CHRIS IF SIDEBAR CAN ONLY BE ON MOBILE??
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
 			<body>
 				<Container>
-					<Sidebar />
+					<Header />
+					{/* <Sidebar /> */}
 					<Content>{children}</Content>
+					<Footer />
 				</Container>
 			</body>
 		</html>
