96af67ceb205e2a21fa1133f2ea83c56c0752e3c
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-11-18 13:57:21 -0800

    removed content

--- src/components/Content/Content.tsx
+++
@@ -1,9 +0,0 @@
-// import styles from './Content.module.css'
-
-// interface Props {
-//   children: React.ReactNode
-// }
-
-// export default function Content({ children }: Props) {
-//   return <section className={styles.content}>{children}</section>
-// }
