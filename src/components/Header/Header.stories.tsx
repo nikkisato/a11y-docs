eb3f0cf4b289ee083bc964924cdb853471ae4686
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2024-06-13 22:38:35 -0700

    Commenting out header stories

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -1,31 +1,31 @@
 // src/components/Button.stories.tsx
 
-import { Meta, StoryObj } from '@storybook/react'
-import React from 'react'
+// import { Meta, StoryObj } from '@storybook/react'
+// import React from 'react'
 
-import Header from './Header'
+// import Header from './Header'
 
-const meta = {
-  title: 'Components/Header',
-  component: Header,
-  parameters: {
-    layout: 'centered',
-  },
-  tags: ['autodocs'],
-} satisfies Meta<typeof Header>
+// const meta = {
+//   title: 'Components/Header',
+//   component: Header,
+//   parameters: {
+//     layout: 'centered',
+//   },
+//   tags: ['autodocs'],
+// } satisfies Meta<typeof Header>
 
-export default meta
+// export default meta
 
-type Story = StoryObj<typeof meta>
+// type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Default: Story = {
-  args: {
-    isStatic: true,
-    isSticky: false,
-    isTransparent: false,
-  },
-}
+// export const Default: Story = {
+//   args: {
+//     isStatic: true,
+//     isSticky: false,
+//     isTransparent: false,
+//   },
+// }
 
 // export const Static: Story = {
 //   args: {
@@ -35,18 +35,22 @@ export const Default: Story = {
 //   },
 // }
 
-export const Sticky: Story = {
-  args: {
-    isStatic: false,
-    isSticky: true,
-    isTransparent: false,
-  },
-}
+// export const Sticky: Story = {
+//   args: {
+//     isStatic: false,
+//     isSticky: true,
+//     isTransparent: false,
+//   },
+// }
 
-export const Transparent: Story = {
-  args: {
-    isStatic: false,
-    isSticky: false,
-    isTransparent: true,
-  },
-}
+// export const Transparent: Story = {
+//   args: {
+//     isStatic: false,
+//     isSticky: false,
+//     isTransparent: true,
+//   },
+// }
+
+// export default {}
+// eslint-disable-next-line storybook/story-exports
+export default {}
